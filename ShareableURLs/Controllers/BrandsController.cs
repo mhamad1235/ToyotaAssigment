d8ea8f1cd10d1fad5adc83e32f28f4504dd641a8
Aza Asim <aza@shift.software>
2024-02-13 03:04:54 +0300

    Add Dummy Share endpoint

--- ShareableURLs/Controllers/BrandsController.cs
+++ ShareableURLs/Controllers/BrandsController.cs
@@ -2,42 +2,44 @@
 using ShareableURLs.Data;
 using ShareableURLs.DTOs;
 
-namespace ShareableURLs.Controllers
+namespace ShareableURLs.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BrandsController : ControllerBase
+    private readonly BrandRepository repository;
+    public BrandsController(BrandRepository brandRepository)
     {
-        public readonly BrandRepository repository;
-        public BrandsController(BrandRepository brandRepository)
-        {
-            this.repository = brandRepository;
-        }
+        this.repository = brandRepository;
+    }
 
-        [HttpGet("")]
-        public ActionResult<BrandDTO> List()
-        {
-            return Ok(this.repository.List());
-        }
+    [HttpGet("")]
+    public ActionResult<BrandDTO> List()
+    {
+        return Ok(this.repository.List());
+    }
 
-        [HttpGet("{id}")]
-        public ActionResult<BrandDTO> Find(long id)
-        {
-            var item = this.repository.Find(id);
+    [HttpGet("{id}")]
+    public ActionResult<BrandDTO> Find(long id)
+    {
+        var item = this.repository.Find(id);
 
-            if (item is null)
-                return NotFound();
+        if (item is null)
+            return NotFound();
 
-            return Ok(item);
-        }
+        return Ok(item);
+    }
 
-        [HttpGet("{id}/get-shareable-url")]
-        public ActionResult<string> GetShareableURL(long id)
-        {
-            //This is returning the URL for Find. Which is not Shareable (Because it requires authentication)
-            //Your task is to return a URL that grants access to this resource for 15 minutes.
+    [HttpGet("{id}/get-shareable-url")]
+    public ActionResult<string> GetShareableURL(long id)
+    {
+        return Ok(Request.Host + Url.Action(nameof(Share), new { id = id }));
+    }
 
-            return $"{Request.Host}/api/brands/{id}";
-        }
+    [HttpGet("{id}/share")]
+    public ActionResult<BrandDTO> Share(long id)
+    {
+        return Find(id);
     }
 }
--- ShareableURLs/Controllers/ProductsController.cs
+++ ShareableURLs/Controllers/ProductsController.cs
@@ -2,42 +2,44 @@
 using ShareableURLs.Data;
 using ShareableURLs.DTOs;
 
-namespace ShareableURLs.Controllers
+namespace ShareableURLs.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly ProductRepository repository;
+    public ProductsController(ProductRepository brandRepository)
     {
-        public readonly ProductRepository repository;
-        public ProductsController(ProductRepository brandRepository)
-        {
-            this.repository = brandRepository;
-        }
+        this.repository = brandRepository;
+    }
 
-        [HttpGet("")]
-        public ActionResult<ProductDTO> List()
-        {
-            return Ok(this.repository.List());
-        }
+    [HttpGet("")]
+    public ActionResult<ProductDTO> List()
+    {
+        return Ok(this.repository.List());
+    }
 
-        [HttpGet("{id}")]
-        public ActionResult<ProductDTO> Find(long id)
-        {
-            var item = this.repository.Find(id);
+    [HttpGet("{id}")]
+    public ActionResult<ProductDTO> Find(long id)
+    {
+        var item = this.repository.Find(id);
 
-            if (item is null)
-                return NotFound();
+        if (item is null)
+            return NotFound();
 
-            return Ok(item);
-        }
+        return Ok(item);
+    }
 
-        [HttpGet("{id}/get-shareable-url")]
-        public ActionResult<string> GetShareableURL(long id)
-        {
-            //This is returning the URL for Find. Which is not Shareable (Because it requires authentication)
-            //Your task is to return a URL that grants access to this resource for 15 minutes.
+    [HttpGet("{id}/get-shareable-url")]
+    public ActionResult<string> GetShareableURL(long id)
+    {
+        return Ok(Request.Host + Url.Action(nameof(Share), new { id = id }));
+    }
 
-            return $"{Request.Host}/api/products/{id}";
-        }
+    [HttpGet("{id}/share")]
+    public ActionResult<ProductDTO> Share(long id)
+    {
+        return Find(id);
     }
 }
